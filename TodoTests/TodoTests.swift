3686076925b673ea3251b7efb48e7c3dcc0593a0
Jacky Tjoa <jacky.coolheart@gmail.com>
2021-04-17 16:07:48 +0800

    add unit tests

--- TodoTests/TodoTests.swift
+++ TodoTests/TodoTests.swift
@@ -10,25 +10,50 @@ import XCTest
 @testable import Todo
 
 class TodoTests: XCTestCase {
+    
+    var sut: ViewModel!
+    let mockService = MockService()
+    
+    class MockService: ServiceProtocol {
+        var isGetTodoListCalled = false
+        var isAddItemCalled = false
+        var isRemoveItemCalled = false
+        
+        func getTodoList() -> Observable<[Todo]> {
+            isGetTodoListCalled = true
+            return Observable(value: [])
+        }
+        
+        func addItem(task: String) {
+            isAddItemCalled = true
+        }
+        
+        func removeItem(id: String) {
+            isRemoveItemCalled = true
+        }
+    }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = ViewModel(service: mockService)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testIsGetToDoListCalled() {
+        XCTAssertTrue(mockService.isGetTodoListCalled)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testIsAddItemCalled() {
+        sut.addItem(task: "My Task")
+        XCTAssertTrue(mockService.isAddItemCalled)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testIsRemoveItemCalled() {
+        sut.removeItem(id: "SomeId")
+        XCTAssertTrue(mockService.isRemoveItemCalled)
     }
-
 }
